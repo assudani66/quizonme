ae4df9015ba8c19a0816185ceb729b88a82a0a5f
PiyushAssudani <63794264+assudani66@users.noreply.github.com>
2021-09-06 15:43:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var readlinesync = require("readline-sync")
+console.log("Lets see how better you know me ??");
+var score = 0
+var quizquestions = [
+  [
+    question = "What is my birthday date? (Only Enter the date like 2,3,4,5)",
+    answer = 21,] ,
+  [
+    question = "What is my birth Year",
+   answer = 2000,]
+  [
+    question = "What Anime series do I love the most",
+   answer = "Attack on titans"],
+[
+    question = "What is the game I love most",
+   answer = "valorant"],
+
+   //Try to add multiple answer funtionalitiy
+
+]
+
+
+console.log(quizquestions[0]);
